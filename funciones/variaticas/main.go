be1c4705e20c0af132c002b50c53077f4105d8ae
Jhon Guerrero <jhoguer@gmail.com>
2020-08-19 09:30:50 -0500

    Iniciando con paquetes

--- funciones/variaticas/main.go
+++ funciones/variaticas/main.go
@@ -7,6 +7,9 @@ func main() {
 	fmt.Println(sum(1, 2, 5, 7))
 }
 
+// la funcion recibe cualquier cantidad de argumentos
+// cada arguumento se añade a un slice
+// se debe operar sabiendo que es un slice
 func sum(nums ...int) int {
 	total := 0
 
---
+++ paquetes/greet/spanish.go
@@ -0,0 +1,6 @@
+package greet
+
+// Spanish retora saludo en español
+func Spanish() string {
+	return "Hola " + emoji
+}
